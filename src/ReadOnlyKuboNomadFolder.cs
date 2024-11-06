5fc59b6286d821e4d7560478b277773c48abe4c5
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-11-05 19:22:46 -0600

    Checkpoint: porting to OwlCore.Nomad.Kubo repository pattern

--- src/ReadOnlyKuboNomadFolder.cs
+++ src/ReadOnlyKuboNomadFolder.cs
@@ -2,9 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using CommunityToolkit.Diagnostics;
 using Ipfs;
 using Ipfs.CoreApi;
 using OwlCore.ComponentModel;
+using OwlCore.Nomad.Kubo;
 using OwlCore.Nomad.Storage.Models;
 using OwlCore.Storage;
 
@@ -15,6 +17,26 @@ namespace OwlCore.Nomad.Storage.Kubo;
 /// </summary>
 public class ReadOnlyKuboNomadFolder : IChildFolder, IDelegable<NomadFolderData<Cid>>, IGetRoot
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="ReadOnlyKuboNomadFolder"/> from the given handler configuration.
+    /// </summary>
+    /// <param name="handlerConfig">The handler configuration to use.</param>
+    /// <param name="parent">The parent of this folder, if any.</param>
+    /// <param name="client">A client that can be used for accessing ipfs.</param>
+    /// <returns>A new instance of <see cref="ReadOnlyKuboNomadFolder"/>.</returns>
+    public static ReadOnlyKuboNomadFolder FromHandlerConfig(NomadKuboEventStreamHandlerConfig<NomadFolderData<Cid>> handlerConfig, IFolder? parent, ICoreApi client)
+    {
+        Guard.IsNotNull(handlerConfig.RoamingValue);
+        Guard.IsNotNull(handlerConfig.RoamingId);
+
+        return new ReadOnlyKuboNomadFolder
+        {
+            Parent = parent,
+            Client = client,
+            Inner = handlerConfig.RoamingValue,
+        };
+    }
+    
     /// <summary>
     /// The client to use for communicating with ipfs/kubo.
     /// </summary>
@@ -32,7 +54,7 @@ public class ReadOnlyKuboNomadFolder : IChildFolder, IDelegable<NomadFolderData<
     /// <summary>
     /// The parent for this folder, if any.
     /// </summary>
-    public required ReadOnlyKuboNomadFolder? Parent { get; init; }
+    public required IFolder? Parent { get; init; }
     
     /// <inheritdoc />
     public async IAsyncEnumerable<IStorableChild> GetItemsAsync(StorableType type = StorableType.All, [EnumeratorCancellation] CancellationToken cancellationToken = default)
@@ -80,11 +102,14 @@ public class ReadOnlyKuboNomadFolder : IChildFolder, IDelegable<NomadFolderData<
         // At least one parent is required for a root to exist
         // Crawl up and return where parent is null
         var current = this;
-        while (current.Parent is { } parent)
+        while (current.Parent is ReadOnlyKuboNomadFolder parent)
         {
             current = parent;
         }
 
+        if (current.Parent is IStorableChild storableChild)
+            return storableChild.GetRootAsync(cancellationToken);
+            
         return Task.FromResult<IFolder?>(current);
     }
 }
